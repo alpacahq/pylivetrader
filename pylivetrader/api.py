b7dcc259e72cd7ba2495b322da04c83c0826779d
yoshiso <nya060@gmail.com>
2018-08-07 17:51:20 -0700

    Extended API which is already implemented

--- pylivetrader/api.py
+++ pylivetrader/api.py
@@ -3,5 +3,19 @@
 All the algorithm API redirections are going to be registered by misc.api_context#api_method method on the fly.
 '''
 
+from pylivetrader.finance import execution
+from pylivetrader.misc import events, math_utils
+from pylivetrader.misc.events import (
+    calendars,
+    date_rules,
+    time_rules
+)
 
-__all__ = []
+__all__ = [
+    'calendars',
+    'date_rules',
+    'time_rules',
+    'events',
+    'execution',
+    'math_utils',
+]
