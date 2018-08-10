7328a9f3b2aa331dd7b745be61127b78a85de650
yoshiso <nya060@gmail.com>
2018-08-10 13:18:44 -0700

    Align prop name in data portal

--- pylivetrader/data/data_portal.py
+++ pylivetrader/data/data_portal.py
@@ -21,13 +21,13 @@ log = Logger('DataPortal')
 
 class DataPortal:
 
-    def __init__(self, broker, asset_finder, trading_calendar):
-        self.broker = broker
+    def __init__(self, backend, asset_finder, trading_calendar):
+        self.backend = backend
         self.asset_finder = asset_finder
         self.trading_calendar = trading_calendar
 
     def get_last_traded_dt(self, asset, dt, data_frequency):
-        return self.broker.get_last_traded_dt(asset)
+        return self.backend.get_last_traded_dt(asset)
 
     def get_adjusted_value(
             self,
@@ -40,14 +40,14 @@ class DataPortal:
         TODO:
         for external data (fetch_csv) support, need to update logic here.
         '''
-        return self.broker.get_spot_value(assets, field, dt, data_frequency)
+        return self.backend.get_spot_value(assets, field, dt, data_frequency)
 
     def get_spot_value(self, assets, field, dt, data_frequency):
-        return self.broker.get_spot_value(assets, field, dt, data_frequency)
+        return self.backend.get_spot_value(assets, field, dt, data_frequency)
 
     @lru_cache(10)
     def _get_realtime_bars(self, assets, frequency, bar_count, end_dt):
-        return self.broker.get_bars(
+        return self.backend.get_bars(
             assets, frequency, bar_count=bar_count)
 
     def cache_clear(self):
@@ -65,7 +65,7 @@ class DataPortal:
         # convert list of asset to tuple of asset to be hashable
         assets = tuple(assets)
 
-        # Broker.get_realtime_history() returns the asset as level 0 column,
+        # Backend.get_bars() returns the asset as level 0 column,
         # open, high, low, close, volume returned as level 1 columns.
         # To filter for field the levels needs to be swapped
         bars = self._get_realtime_bars(
