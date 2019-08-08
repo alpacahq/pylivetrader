ed8d3ceaa88cb290cce381fa66f70cc1c39b06dd
ttt733 <trevor.thackston@gmail.com>
2019-08-08 11:48:47 -0500

    Remove Python 3.5 from supported languages

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Operating System :: OS Independent',
